42efa02ed5138a640c918d129c245bdc203b5fb2
1cloud Marchuk <telefonnomiil4510@gmail.com>
2026-01-30 20:17:59 -0800

    addModal_close_button

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,8 +8,31 @@ function creatCard(chuvak) {
   text.className = "text";
   text.textContent = chuvak.name;
   card.append(img, text);
+  card.addEventListener(`click`, () => {
+    addModal(chuvak);
+  });
   return card;
 }
+
+const modal = document.getElementById("modal_id");
+const close = document.querySelector(".close");
+const modal_img = document.getElementById("modal_img");
+const title_m = document.getElementById("title_m");
+function addModal(chuvak) {
+  modal_img.src = chuvak.image;
+  title_m.textContent = chuvak.name;
+  modal.style.display = "flex";
+}
+
+modal.addEventListener(`click`, (e) => {
+  if (e.target === modal) {
+    modal.style = "none";
+  }
+});
+modal.addEventListener(`click`, (e) => {
+  modal.style = "none";
+});
+
 const grid = document.getElementById(`grid`);
 async function getPok(count) {
   grid.innerHTML = ` `;
@@ -21,6 +44,10 @@ async function getPok(count) {
 }
 getPok(20);
 
+// function addModal(chuvak) {
+//   const modalka = document.getElementById(`modal`);
+
+// }
 // fetch(`http://localhost:3000`)
 //   .then((resp) => {
 //     if (!resp.ok) {
